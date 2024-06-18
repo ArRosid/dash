dda9e168482c43497f4e237b20fe45aed23f67cc
Ahmad Rosid Komarudin <arrosid@192.168.1.4>
2024-06-18 15:45:01 +0700

    add listen 0.0.0.0

--- apps.js
+++ apps.js
@@ -35,7 +35,7 @@ import('node-fetch').then(fetch => {
       }
   });
   
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server is running on http://localhost:${PORT}`);
   });
 }).catch(error => {
